b606e9377843ec3280e39ff6a5a49cb1ce8d15bc
jkendalldev <jason@jkendl.net>
2021-04-23 23:38:59 -0400

    add makeArrayConsecutive.py

--- python/makeArrayConsecutive.py
+++ python/makeArrayConsecutive.py
@@ -1,10 +1,19 @@
 #!/usr/local/bin/python3
 
-def shapeArea(n):
-  sa = ((n -1) * (n -1)) + (n * n)
-  return sa
+def makeArrayConsecutive2(statues):
+  sorted_statues = sorted(statues)
+  missing_statues = 0
+  increase = 0
+  for count, value in enumerate(sorted_statues):
+    if count != 0:
+      increase = value - sorted_statues[count -1]
+      if increase != 1:
+        missing_statues = missing_statues + (increase - 1)
+  return missing_statues   
 
-print(shapeArea(1))
+  
+statues = [6, 2, 3, 8]
+print(makeArrayConsecutive2(statues))
 
 
  
