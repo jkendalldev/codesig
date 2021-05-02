b6a55a6640e9daafdf0adc48b462f3f3e7377384
jkendalldev <jason@jkendl.net>
2021-05-02 16:15:44 -0400

    add almostIncreasingSequence.py

--- python/almostIncreasingSequence.py
+++ python/almostIncreasingSequence.py
@@ -1,23 +1,18 @@
 #!/usr/local/bin/python3
 
 def almostIncreasingSequence(sequence):
-  isAlmostIncreasing = True
-  threw_out_a_number_count = 0
-  
+  isAlmostIncreasing = True 
+  was_not_increased_count = 0
+
   for count, value in enumerate(sequence):
     if count > 0:
-      previous_num = sequence[count -1]
-      if value <= previous_num:
-        threw_out_a_number_count += 1
-        # only do this if we are NOT on a count of 1...
-        if count != 1 and count + 1 < (len(sequence)):
-          next_num = sequence[count +1]
-          if next_num <= previous_num:
-            isAlmostIncreasing = False
-           
-  if threw_out_a_number_count > 1:
+      if value <= sequence[count -1]:
+        was_not_increased_count += 1
+
+  if was_not_increased_count > 1:
     isAlmostIncreasing = False
   
+  #print(was_not_increased_count)
   return isAlmostIncreasing  
 
 #sequence = [10, 1, 2, 3, 4, 5] 
@@ -25,7 +20,9 @@ def almostIncreasingSequence(sequence):
 #sequence = [1, 2, 1, 2]
 #sequence = [1, 2, 3, 5, 7, 9, 22]
 #sequence = [1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6]
-sequence = [1, 2, 5, 3, 5]
+# sequence = [1, 2, 5, 3, 5]
+#sequence = [1, 2, 3, 4, 99, 5, 6]
+# sequence = [1, 3, 2, 1]
 
 print(almostIncreasingSequence(sequence))
 
