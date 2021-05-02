9bfb5788baa482630a2b70cff799b9d25195e443
jkendalldev <jason@jkendl.net>
2021-05-02 10:54:17 -0400

    add almostIncreasingSequence.py

--- python/almostIncreasingSequence.py
+++ python/almostIncreasingSequence.py
@@ -1,19 +1,31 @@
 #!/usr/local/bin/python3
 
-def makeArrayConsecutive2(statues):
-  sorted_statues = sorted(statues)
-  missing_statues = 0
-  increase = 0
-  for count, value in enumerate(sorted_statues):
-    if count != 0:
-      increase = value - sorted_statues[count -1]
-      if increase != 1:
-        missing_statues = missing_statues + (increase - 1)
-  return missing_statues   
+# Given a sequence of integers as an array, determine whether it is possible to
+#  obtain a strictly increasing sequence by removing no more than one element from
+#  the array.
 
-  
-statues = [6, 2, 3, 8]
-print(makeArrayConsecutive2(statues))
+# For sequence = [1, 3, 2, 1], the output should be false
+# almostIncreasingSequence(sequence) = false.
+# There is no one element in this array that can be removed in order to
+#  get a strictly increasing sequence.
+
+def almostIncreasingSequence(sequence):
+  # just count how many times the diff between current number and previous
+  # number is <= 0, if that count is > 1, the return false, otherwise return true!
+  sequence_did_not_increase_count = 0
+  for count, value in enumerate(sequence):
+    if count != 0: #if we are not looking at the very first element in the list
+      change = value - sequence[count -1]
+      if change <= 0:
+        sequence_did_not_increase_count += 1
+  if sequence_did_not_increase_count > 1:
+    return False
+  else:
+    return True
+   
+
+sequence = [1, 5, 3, 1] 
+print(almostIncreasingSequence(sequence))
 
 
  
