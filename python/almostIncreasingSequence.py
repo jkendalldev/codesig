f919c420df0d886867d083495ab9fd8236c197b4
jkendalldev <jason@jkendl.net>
2021-05-02 15:42:28 -0400

    add almostIncreasingSequence.py

--- python/almostIncreasingSequence.py
+++ python/almostIncreasingSequence.py
@@ -1,30 +1,32 @@
 #!/usr/local/bin/python3
 
-# Given a sequence of integers as an array, determine whether it is possible to
-#  obtain a strictly increasing sequence by removing no more than one element from
-#  the array.
-
-# For sequence = [1, 3, 2, 1], the output should be false
-# almostIncreasingSequence(sequence) = false.
-# There is no one element in this array that can be removed in order to
-#  get a strictly increasing sequence.
-
 def almostIncreasingSequence(sequence):
-  # just count how many times the diff between current number and previous
-  # number is <= 0, if that count is > 1, the return false, otherwise return true!
-  sequence_did_not_increase_count = 0
+  isAlmostIncreasing = True
+  threw_out_a_number_count = 0
+  
   for count, value in enumerate(sequence):
-    if count != 0: #if we are not looking at the very first element in the list
-      change = value - sequence[count -1]
-      if change <= 0:
-        sequence_did_not_increase_count += 1
-  if sequence_did_not_increase_count > 1:
-    return False
-  else:
-    return True
-   
+    if count > 0:
+      previous_num = sequence[count -1]
+      if value <= previous_num:
+        threw_out_a_number_count += 1
+        # only do this if we are NOT on a count of 1...
+        if count != 1 and count + 1 < (len(sequence)):
+          next_num = sequence[count +1]
+          if next_num <= previous_num:
+            isAlmostIncreasing = False
+           
+  if threw_out_a_number_count > 1:
+    isAlmostIncreasing = False
+  
+  return isAlmostIncreasing  
 
-sequence = [1, 5, 3, 1] 
+#sequence = [10, 1, 2, 3, 4, 5] 
+#sequence = [1, 3, 2]
+#sequence = [1, 2, 1, 2]
+#sequence = [1, 2, 3, 5, 7, 9, 22]
+#sequence = [1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6]
+sequence = [1, 2, 5, 3, 5]
+
 print(almostIncreasingSequence(sequence))
 
 
