f0cedb2a02bd96a8325bd1588194e49e5bc80552
jkendalldev <jason@jkendl.net>
2021-05-04 10:15:04 -0400

    adding more python

---
+++ python/matrixElementsSum.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python3
+
+#def matrixElementsSum(matrix):
+
+# In Python any table can be represented as a list of lists 
+# (a list, where each element is in turn a list).
+
+#https://snakify.org/en/lessons/two_dimensional_lists_arrays/
+
+m = [[1, 1, 1, 0], [0, 5, 0, 1], [2, 1, 3, 10]]
+
+
+
+#example input:
+#matrix = [[1, 1, 1, 0], 
+#          [0, 5, 0, 1], 
+#          [2, 1, 3, 10]]
+          
+#          [output] integer
+#expected output: 9
+#The total price of all the rooms that are suitable for the CodeBots to live in.
+
+
+
+
---
+++ python/pdb_debug.py
@@ -0,0 +1,3 @@
+import random
+import pdb #Python debugger
+
