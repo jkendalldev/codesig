83aae27ddeee2e9bb4cb1dd73a8f5a047c2f3123
jkendalldev <jason@jkendl.net>
2021-05-05 11:53:39 -0400

    more python coding

--- python/matrixElementsSum.py
+++ python/matrixElementsSum.py
@@ -1,18 +1,15 @@
 #!/usr/local/bin/python3
 
-def matrixElementsSum(matrix):
+def matrixElementsSum(floors):
     cost_of_rooms = 0
     cols = [list(x) for x in zip(*floors)]
     for count, col in enumerate(cols):  
         for cnt, room in enumerate(col):
             if cnt != 0:
                 rooms_above_me = col[:cnt]
-                print(f"I am room {room} , rooms above me are {rooms_above_me} ...")
                 if 0 not in rooms_above_me:
                     cost_of_rooms += room
-                    print("No room above me cost $0")  
     rooms_total = cost_of_rooms + sum(floors[0])
     return rooms_total
-
 floors = [[10, 0, 10, 0], [1, 5, 0, 1], [2, 1, 3, 10]]
 print(matrixElementsSum(floors))
