8624694b6026618fbf2aecff6d1cd1d7157c471b
jkendalldev <jason@jkendl.net>
2021-05-16 08:13:25 -0400

    add allLongestStrings.py

---
+++ python/allLongestStrings.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3
+
+# Given an array of strings, return another array containing all of its longest strings.
+# For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+# allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
+
+def allLongestStrings(inputArray):
+    
+
