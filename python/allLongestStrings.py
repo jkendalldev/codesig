7910a5af6412c9772b226d7f4977c00626ea2cf2
jkendalldev <jason@jkendl.net>
2021-05-16 08:30:13 -0400

    add allLongestStrings.py

--- python/allLongestStrings.py
+++ python/allLongestStrings.py
@@ -3,7 +3,6 @@
 # Given an array of strings, return another array containing all of its longest strings.
 # For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
 # allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
-
 def allLongestStrings(inputArray):
     size = len(max(inputArray, key=len))
     return ([word for word in inputArray if len(word) == size])  #Example of list comprehension
