7b7f1150b07ef2aaf597dc0f1425aa311b3c4a09
jkendalldev <jason@jkendl.net>
2021-07-03 21:16:31 -0400

    events app

--- events_go_app/server.go
+++ events_go_app/server.go
@@ -146,8 +146,35 @@ func ServeAPI(endpoint string, dbHandler persistence.DataHandler) error {
 	return http.ListenAndServe(endpoint, r)
 }
 
+// Configuration Layer..
+package configuration 
+var (
+	DBTypeDefault = dblayer.DBTYPE("mongodb")
+	DBConnectionDefault = "mongodb://127.0.0.1"
+	RestfulEPDefault = "localhost:8181"
+)
+type ServiceConfig struct {
+	Databasetype dblayer.DBTYPE `json:"databasetype"`
+	DBConnection string `json:"dbconnection"`
+	RestfulEndpoint string `json:"restfulapi_endpoint"`
+}
+func ExtractConfiguration(filename string) (ServiceConfig, error){
+	conf := ServiceConfig{
+		DBTypeDefault,
+		DBConnectionDefault,
+		RestfulEPDefault,
+	}
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Configuration file not found. Continuing with default values.")
+		return conf, err 
+	}
+	err = json.NewDecoder(file).Decode(&conf)
+	return conf,err 
+}
 
-// Persistence layer
+
+// Persistence layer..
 type DatabaseHandler interface {
 	AddEvent(Event) ([]byte, error)
 	FindEvent([]byte) (Event, error)
